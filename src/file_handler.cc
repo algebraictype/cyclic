c467cecb13e4e5e74c154a65d8623c606b97883c
ln <a.j.b.0760@gmail.com>
2025-03-04 11:43:15 -0800

    changes to file parsing

--- src/file_handler.cc
+++ src/file_handler.cc
@@ -2,26 +2,33 @@
 #include "../include/file_handler.hh"
 #include "../include/asm_gen.hh"
 
-//read file into memory.
-[[nodiscard]] std::string FileHandler::read_file(const std::string& file_name){
 
+[[nodiscard]] std::string FileHandler::read_file(const std::string& file_name) {
     //open file
-    std::ifstream input_file(file_name);
+    std::ifstream input_file(file_name, std::ios::binary);
+    if (!input_file.is_open()) {
+        throw std::runtime_error("Could not open file --ensure that file is in directory of executable.: " + file_name);
+    }
 
-    //set position file start to file end;
+    //set position to file end to determine file size
     input_file.seekg(0, std::ios::end);
+    std::streampos size = input_file.tellg();
+    if (size <= 0) {
+        throw std::runtime_error("File is empty or error in getting file size: " + file_name);
+    }
 
-    //obtain file size;
-    size_t size = input_file.tellg();
+    //allocate buffer and fill with ' ' chars
+    std::string buffer(static_cast<size_t>(size), ' ');
 
-    //allocate room and fill with ' ' chars.
-    std::string buffer(size, ' ');
-
-    //set position to file start;
+    //set position to file start
     input_file.seekg(0);
 
-    //read buffer of size "size" given address to start of buffer.
+    //read file into buffer
     input_file.read(&buffer[0], size);
+    if (!input_file) {
+        throw std::runtime_error("Error reading file: " + file_name);
+    }
+
     return buffer;
 }
 
--- src/main.cc
+++ src/main.cc
@@ -12,23 +12,29 @@
 #include "../include/error.hh"
 #include "../include/asm_gen.hh"
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
+    if (argc <= 1) {
+        std::cerr << "No file provided!\nUsage: ./cyclic [program.cy]" << std::endl;
+        std::string f = "sm.cy";
+        argv[1] = f.data();
+        //return 1;
+    }
 
-    //if argument is passed, (file_name) read the file into memory.
-    std::cout<<argv[1]<<std::endl;
+    std::cout << argv[1] << std::endl;
 
-
-    if(argv[1] && argc > 0){
-
-        testing::InitGoogleTest(&argc, argv);
-        //return RUN_ALL_TESTS();
+    if (argv[1]) {
         std::string file_name = argv[1];
 
-        if(!file_name.empty()){
-            std::string file_contents = FileHandler::read_file(file_name);
-            //file_contents = file_name;
+        if (!file_name.empty()) {
+            std::string file_contents;
+            try {
+                file_contents = FileHandler::read_file(file_name);
+            } catch (const std::exception& e) {
+                std::cerr << "Error reading file: " << e.what() << std::endl;
+                return 1;
+            }
 
-            //lexing/parsing phase
+            // lexing/parsing phase
             Lexer lexer;
             Parser parser;
             lexer.lex_input(file_contents);
@@ -38,43 +44,23 @@ int main(int argc, char** argv){
             lexer.print();
 #endif
 
-
             AST ast;
             std::vector<std::shared_ptr<ExpressionNode>> expression_roots;
-            for(const auto& token_vector : lexer.get_tokens()){
+            for (const auto& token_vector : lexer.get_tokens()) {
                 auto root_ptr = parser.parse_program(token_vector);
                 expression_roots.push_back(root_ptr);
             }
 
-            for(const auto& root_ptrs : expression_roots){
-                if(root_ptrs){
+            for (const auto& root_ptrs : expression_roots) {
+                if (root_ptrs) {
                     ast.visit(root_ptrs);
-                    std::cout<< " " << std::endl;
+                    std::cout << " " << std::endl;
                 } else {
-                    std::cerr<<"Failed to parse program.\n";
+                    std::cerr << "Failed to parse program.\n";
                 }
             }
-
-            /*
-            Generator asm_gen;
-            AST ast;
-
-            if(auto root_ptr = parser.parse_program()) {
-
-                ast.visit(root_ptr);
-                asm_gen.generate(root_ptr);
-                //asm_gen.print();
-            } else {
-                std::cout << "Failed to parse program.\n";
-            }
-             */
         }
     }
 
-    //flush stream and exit;
-    if(argc <= 1){
-
-        std::cerr<<"No file provided!\nUsage: ./cyclic [program.cy]"<<std::endl;
-        return 1;
-    }
+    return 0;
 }
